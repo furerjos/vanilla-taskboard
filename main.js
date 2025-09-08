7a34721b4a42cc0813d3e5a59d689671185aa79d
Josh Furer <joshuamfurer@gmail.com>
2025-09-08 15:23:27 -0400

    Second session: Externalized CSS, Started creating Taskboard component, will get back to this.

--- main.js
+++ main.js
@@ -4,6 +4,10 @@
 const nameFormElem = document.getElementById("name-setter");
 const nameInHeader = document.getElementById("headername");
 const clearButton = document.getElementById("nameclearbtn");
+const addButton = document.getElementById("addbtn");
+
+const taskboardDiv = document.getElementById("taskboard-div");
+const taskboardHeader = document.getElementById("taskboard-header");
 
 // Upon Initializing
 console.log(localStorage);
@@ -28,15 +32,28 @@ nameFormElem.addEventListener("submit", (e) => {
     : formData.get("inputname").toLowerCase().endsWith("s")
     ? (nameInHeader.textContent = formData.get("inputname") + `'`)
     : (nameInHeader.textContent = formData.get("inputname") + `'s`);
-  localStorage.setItem("inputname", formData.get("inputname"));
-  console.log("Checking local storage: " + localStorage.getItem("inputname"));
+  localStorage.setItem("taskboard:name", formData.get("inputname"));
+  console.log(
+    "Checking local storage: " + localStorage.getItem("taskboard:name")
+  );
 });
 
 clearButton.addEventListener("click", (e) => {
-  localStorage.removeItem("inputname");
+  localStorage.removeItem("taskboard:name");
   console.log(
-    "After clicking clear, inputname is: " + localStorage.getItem("inputname")
+    "After clicking clear, inputname is: " +
+      localStorage.getItem("taskboard:name")
   );
   nameInHeader.textContent = `______'s`;
   document.getElementById("inputname").value = "";
 });
+
+addButton.addEventListener("click", (e) => {
+  const newDiv = document.createElement("div");
+  newDiv.classList.add("task-item");
+  taskboardHeader.appendChild(newDiv);
+});
+
+function createTaskElement() {
+  // will make the task element here
+}
